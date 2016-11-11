634bac533324cd659757afb20ddbedbfcca58daf
ojmme <ojmme625@gmail.com>
2016-11-11 16:16:06 +0900

    make filter function

--- js/plot.js
+++ js/plot.js
@@ -1,13 +1,6 @@
 
 d3.csv("../data/seeddataset2.csv", function(error,data){
-/*	data = data.map(function(d) {
-		return [ +d["Song_id"],
-			+d["Title"]
-			+d["Artist"]
-			+d["Valance"]
-			+d["Arousal"]
-			+d["Genre"]]; });
-*/
+	//read data
 	data.forEach(function(d) 
 	{ d.Song_id = +d.Song_id;
 	  d.Title = d.Title;
@@ -16,39 +9,19 @@ d3.csv("../data/seeddataset2.csv", function(error,data){
 	  d.Arousal = +d.Arousal;
 	  d.Genre = d.Genre});
 
-
+/*
 d3.csv("../data/similardata_final_final.csv", function(error,data2){
+	//read second data
 	data2.forEach(function(d)
 	{ d.seedID = +d.Seed_id;
 	  d.id = +d.id;
 	  d.title = +d.Title;
 	  d.artist = +d.Artist;
 	  d.similarity = +d.Similarity});
-	
+*/
 
 var maxX = 1 + d3.max(data, function(d) {return d.Valance;});
 var maxY = 1 + d3.max(data, function(d) {return d.Arousal;});
-
-/*
-var rand1, rand2;
-
-for(var i=0; i<data.length; i++){
-
-	
-	rand1 = Math.floor(Math.random()*200001 + (-100000));
-	rand2 = Math.floor(Math.random()*200001 + (-100000));
-		
-	if(data[i].valance+rand1 > 0 && data[i].valance+rand1 < maxX)
-		data[i].valance += rand1;
-	else
-		data[i].valance -= rand1;
-
-	if(data[i].arousal+rand2 > 0 && data[i].arousal+rand2 < maxY)
-		data[i].arousal += rand2;
-	else
-		data[i].arousal -= rand2;
-}
-*/
 
 var w = 1590;
 var h = 655;
@@ -61,24 +34,6 @@ var yScale = d3.scaleLinear()
 		.domain([0, maxY])
 		.range([h-padding, padding]);
 
-/*
-var xAxis = d3.axisBottom()
-    .scale(xScale)
-    .ticks(5);
-
-var yAxis = d3.axisLeft()
-    .scale(yScale)
-    .ticks(5);
-*/
-
-
-var tooltip = d3.select("#graph")
-	.append("div")
-	.attr("class", "d3-tip")
-	.append("div")
-	.attr("id", "artist")
-	.append("div")
-	.attr("id", "title");
 
 var svg = d3.select("#graph")
     .append("svg")
@@ -93,44 +48,46 @@ svg.selectAll("text")
   .attr("y", h/2)
   .attr("text-anchor", "middle");
 
-svg.selectAll("circle")
-	.data(data)
-	.enter()
-	.append("circle")
-	.attr("cx", function(d) {return xScale(d.Valance);})
-	.attr("cy", function(d) {return yScale(d.Arousal);})
-	.attr("r", 3)
-	.attr("fill", "black")
-	.on("mouseover", function(d) {
-		d3.select(this)
-			.attr("fill", "cyan");
+	drawPlot(data);
 
-		var xPosition = parseFloat(d3.select(this).attr("x"))+10;
-		var yPosition = parseFloat(d3.select(this).attr("y"))/2;
-/*
-		tooltip.select("#artist").html(d.artist)
-			.select("#title").html(d.title)
-			.style("left", xPosition + "px")
-			.style("top", yPosition + "px")
-			.style("display", "block");
-*/	})
-	.on("mouseout", function() {
-		d3.select(this)
-			.attr("fill", "black");
+	function drawPlot(dataSet) {
 
-		tooltip.style("display", "none");
-	})
-	.on("click", function(d) {
-		var center = d.Song_id;
-		dataList[0] = center;
+		svg.selectAll("circle")
+			.data(dataSet)
+			.enter()
+			.append("circle")
+			.attr("cx", function (d) {
+				return xScale(d.Valance);
+			})
+			.attr("cy", function (d) {
+				return yScale(d.Arousal);
+			})
+			.attr("r", 3)
+			.attr("fill", "black")
+			.on("mouseover", function (d) {
+				d3.select(this)
+					.attr("fill", "cyan");
 
-		//put similar datas
-		for(var i=1; i<6; i++){
-			dataList[i] = i;
-		}
+				var xPosition = parseFloat(d3.select(this).attr("x")) + 10;
+				var yPosition = parseFloat(d3.select(this).attr("y")) / 2;
+			})
+			.on("mouseout", function () {
+				d3.select(this)
+					.attr("fill", "black");
+			})
+			.on("click", function (d) {
+				var center = d.Song_id;
+				dataList[0] = center;
 
-		drawGraph(dataList);
-});
+				//put similar datas
+				for (var i = 1; i < 6; i++) {
+					dataList[i] = i;
+				}
+
+				drawGraph(dataList);
+			});
+		filter();
+	}
 
 	function drawGraph(dataList){
 		var w = 1590;
@@ -156,20 +113,24 @@ svg.selectAll("circle")
 			.attr("cy", h/2)
 			.attr("r", 30)
 			.attr("fill", "orange");
+	}
+
+	function filter(){
+		var filteredData = [];
+		var genre = document.getElementById('pop');
+		genre.addEventListener('click', function(){
+			alert('Hello world');
+
+			for(var i=0; i<length.data; i++){
+				if(data[i].Genre == "pop")
+					filteredData.push(data[i]);
+					console.log(i);
+			}
+			console.log(length.filteredData);
+			drawPlot(filteredData);
+		})
 
 	}
 
-/*
-svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(0," + (h-padding) +")")
-	.call(xAxis);
-
-svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(" + padding + ",0)")
-	.call(yAxis);
-*/
-});
 });
 
