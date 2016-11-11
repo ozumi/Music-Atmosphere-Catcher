69edb27ae28d6a5bc0a265b96401af5fe208b9a6
ojmme <ojmme625@gmail.com>
2016-11-11 18:12:02 +0900

    filter function change

--- js/plot.js
+++ js/plot.js
@@ -1,58 +1,51 @@
-
-d3.csv("../data/seeddataset2.csv", function(error,data){
+d3.csv("../data/seeddataset2.csv", function(error,data) {
 	//read data
-	data.forEach(function(d) 
-	{ d.Song_id = +d.Song_id;
-	  d.Title = d.Title;
-	  d.Artist = d.Artist;
-	  d.Valance = +d.Valance;
-	  d.Arousal = +d.Arousal;
-	  d.Genre = d.Genre});
+	data.forEach(function (d) {
+		d.Song_id = +d.Song_id;
+		d.Title = d.Title;
+		d.Artist = d.Artist;
+		d.Valance = +d.Valance;
+		d.Arousal = +d.Arousal;
+		d.Genre = d.Genre
+	});
 
-/*
-d3.csv("../data/similardata_final_final.csv", function(error,data2){
-	//read second data
-	data2.forEach(function(d)
-	{ d.seedID = +d.Seed_id;
-	  d.id = +d.id;
-	  d.title = +d.Title;
-	  d.artist = +d.Artist;
-	  d.similarity = +d.Similarity});
-*/
+		var maxX = 1 + d3.max(data, function (d) {
+				return d.Valance;
+			});
+		var maxY = 1 + d3.max(data, function (d) {
+				return d.Arousal;
+			});
 
-var maxX = 1 + d3.max(data, function(d) {return d.Valance;});
-var maxY = 1 + d3.max(data, function(d) {return d.Arousal;});
+		var w = 1590;
+		var h = 655;
+		var padding = 20;
+		var dataList = [0, 0, 0, 0, 0, 0];
+		var xScale = d3.scaleLinear()
+			.domain([0, maxX])
+			.range([padding, w - padding]);
+		var yScale = d3.scaleLinear()
+			.domain([0, maxY])
+			.range([h - padding, padding]);
 
-var w = 1590;
-var h = 655;
-var padding = 20;
-var dataList = [0,0,0,0,0,0];
-var xScale = d3.scaleLinear()
-		.domain([0, maxX])
-		.range([padding, w-padding]);
-var yScale = d3.scaleLinear()
-		.domain([0, maxY])
-		.range([h-padding, padding]);
+		var svg = d3.select("#graph")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
 
+		drawPlot(data);
 
-var svg = d3.select("#graph")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-svg.selectAll("text")
-  .enter()
-  .append("text")
-  .text("Positive")
-  .attr("x", w-padding)
-  .attr("y", h/2)
-  .attr("text-anchor", "middle");
-
-	drawPlot(data);
 
 	function drawPlot(dataSet) {
 
-		svg.selectAll("circle")
+		svg.select("#circles").remove();
+
+		var circles = svg
+			.append("g")
+			.attr("id", "circles")
+			.attr("width", w)
+			.attr("height", h);
+
+		circles.selectAll("circle")
 			.data(dataSet)
 			.enter()
 			.append("circle")
@@ -68,8 +61,8 @@ svg.selectAll("text")
 				d3.select(this)
 					.attr("fill", "cyan");
 
-				var xPosition = parseFloat(d3.select(this).attr("x")) + 10;
-				var yPosition = parseFloat(d3.select(this).attr("y")) / 2;
+			//	var xPosition = parseFloat(d3.select(this).attr("x")) + 10;
+			//	var yPosition = parseFloat(d3.select(this).attr("y")) / 2;
 			})
 			.on("mouseout", function () {
 				d3.select(this)
@@ -82,26 +75,29 @@ svg.selectAll("text")
 				//put similar datas
 				for (var i = 1; i < 6; i++) {
 					dataList[i] = i;
+					//data 의 id가 center인 애으ㅣ
+					//dataList[1] = d.first;
+					//dataList[2] = d.second;
+					//
 				}
 
 				drawGraph(dataList);
 			});
-		filter();
 	}
 
-	function drawGraph(dataList){
-		var w = 1590;
-		var h = 655;
+	filter();
 
-		svg.select("group").remove();
+	function drawGraph(dataList){
+
+		svg.select("#group").remove();
 
 		var netGroup = svg
 			.append("g")
 			.attr("id", "group")
 			.attr("x", 0)
 			.attr("y", 0)
-			.attr("height", 100)
-			.attr("width", 100);
+			.attr("height", h)
+			.attr("width", w);
 
 		var networks = netGroup
 			.selectAll("circle")
@@ -117,20 +113,27 @@ svg.selectAll("text")
 
 	function filter(){
 		var filteredData = [];
-		var genre = document.getElementById('pop');
-		genre.addEventListener('click', function(){
-			alert('Hello world');
+		//while (filteredData.length) { filteredData.pop(); }
+		var genres = document.getElementsByName("genre");
+		//console.log(genres.length);
+		for(var i in genres.length){
+			console.log(genres.item[i].Artist);
+			genres[i].onclick = function() {
+				var title = genres.item[i].getAttribute("title");
 
-			for(var i=0; i<length.data; i++){
-				if(data[i].Genre == "pop")
-					filteredData.push(data[i]);
-					console.log(i);
-			}
-			console.log(length.filteredData);
-			drawPlot(filteredData);
-		})
-
+				//genre.addEventListener('click', function(){
+				console.log(title);
+				/*
+				 for(var i=0; i<data.length; i++){
+				 console.log(data[i].Genre);
+				 if(data[i].Genre == title) {
+				 filteredData.push(data[i]);
+				 }
+				 }
+				 console.log(filteredData.length);
+				 drawPlot(filteredData);*/
+			}}
+		//})
 	}
-
 });
 
